0000682fc880c057d8724a810c3b3c86832024e5
Raghu Betina <raghu@firstdraft.com>
2017-03-11 20:40:37 +0000

    Direct association between games and gameratings

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,9 @@
 class Game < ApplicationRecord
   # Direct associations
 
+  has_many   :gameratings,
+             :dependent => :destroy
+
   has_many   :comments,
              :dependent => :destroy
 
