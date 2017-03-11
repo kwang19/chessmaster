0f9d62bb0f8a4bff7764aabd10a46d9a891c5f16
Raghu Betina <raghu@firstdraft.com>
2017-03-11 20:40:14 +0000

    Generate games resource with starter:resource

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,91 @@
+class GamesController < ApplicationController
+  def index
+    @games = Game.all
+
+    render("games/index.html.erb")
+  end
+
+  def show
+    @game = Game.find(params[:id])
+
+    render("games/show.html.erb")
+  end
+
+  def new
+    @game = Game.new
+
+    render("games/new.html.erb")
+  end
+
+  def create
+    @game = Game.new
+
+    @game.pgn = params[:pgn]
+    @game.user_id = params[:user_id]
+    @game.eventname = params[:eventname]
+    @game.eventdate = params[:eventdate]
+    @game.white = params[:white]
+    @game.black = params[:black]
+    @game.result = params[:result]
+
+    save_status = @game.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/games/new", "/create_game"
+        redirect_to("/games")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Game created successfully.")
+      end
+    else
+      render("games/new.html.erb")
+    end
+  end
+
+  def edit
+    @game = Game.find(params[:id])
+
+    render("games/edit.html.erb")
+  end
+
+  def update
+    @game = Game.find(params[:id])
+
+    @game.pgn = params[:pgn]
+    @game.user_id = params[:user_id]
+    @game.eventname = params[:eventname]
+    @game.eventdate = params[:eventdate]
+    @game.white = params[:white]
+    @game.black = params[:black]
+    @game.result = params[:result]
+
+    save_status = @game.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/games/#{@game.id}/edit", "/update_game"
+        redirect_to("/games/#{@game.id}", :notice => "Game updated successfully.")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Game updated successfully.")
+      end
+    else
+      render("games/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @game = Game.find(params[:id])
+
+    @game.destroy
+
+    if URI(request.referer).path == "/games/#{@game.id}"
+      redirect_to("/", :notice => "Game deleted.")
+    else
+      redirect_back(:fallback_location => "/", :notice => "Game deleted.")
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  # Routes for the Game resource:
+  # CREATE
+  get "/games/new", :controller => "games", :action => "new"
+  post "/create_game", :controller => "games", :action => "create"
+
+  # READ
+  get "/games", :controller => "games", :action => "index"
+  get "/games/:id", :controller => "games", :action => "show"
+
+  # UPDATE
+  get "/games/:id/edit", :controller => "games", :action => "edit"
+  post "/update_game/:id", :controller => "games", :action => "update"
+
+  # DELETE
+  get "/delete_game/:id", :controller => "games", :action => "destroy"
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
