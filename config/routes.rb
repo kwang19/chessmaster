323cedb992d17e24b2ac21f3c81a391191154a9e
Raghu Betina <raghu@firstdraft.com>
2017-03-11 20:40:28 +0000

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => "games#index"
   # Routes for the Gamerating resource:
   # CREATE
   get "/gameratings/new", :controller => "gameratings", :action => "new"
