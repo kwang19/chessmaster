7508ce9f9abf03df1a0c1a49213b204086c5965d
Raghu Betina <raghu@firstdraft.com>
2017-03-11 20:40:55 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "games#index"
   # Routes for the Gamerating resource:
   # CREATE
